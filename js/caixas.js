007b9a1efc49aedc707efd7817c26692b9690f34
GamerCleanVic <jonassd2000@gmail.com>
2022-06-28 21:39:54 -0500

    AlgSomaAté<200

--- js/caixas.js
+++ js/caixas.js
@@ -17,9 +17,9 @@ function enviar(){
 	const h2TT = document.getElementById("tituloCaixa1");
 
 	var aux1 = document.getElementById("caixa1").value;
-	var aux2 = document.getElementById("caixa2").value;
-	var aux3 = document.getElementById("caixa2").value;
-	var aux4 = document.getElementById("caixa2").value;
+	//var aux2 = document.getElementById("caixa2").value;
+	//var aux3 = document.getElementById("caixa2").value;
+	//var aux4 = document.getElementById("caixa2").value;
 	
 	if(document.getElementById("caixa1").value == ""){
 		document.write("<a href='index.html' style='text-align:center; text-decoration:none; width:400px; height:80px; font-weight:bold; font-size:25px; display:flex; flex-direction:row; justify-content:center; align-items:center; background-color:#6600ff; color:#d9d9d9; border-radius:5px; border:1px solid #6600ff; box-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px #d9d9d9;'>CLICK E DIGITE ALGO!</a>");
@@ -27,36 +27,33 @@ function enviar(){
 	else if(document.getElementById("caixa1").value == 0){
 		document.write("<a href='index.html' style='text-align:center; text-decoration:none; width:400px; height:80px; font-weight:bold; font-size:25px; display:flex; flex-direction:row; justify-content:center; align-items:center; background-color:#6600ff; color:#d9d9d9; border-radius:5px; border:1px solid #6600ff; box-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px #d9d9d9;'>CLICK E DIGITE 1 Nº INTEIRO DIFERENTE DE ZERO!</a>");
 	}
-	else if(document.getElementById("caixa2").value == ""){
+	/* else if(document.getElementById("caixa2").value == ""){
 		document.write("<a href='index.html' style='text-align:center; text-decoration:none; width:400px; height:80px; font-weight:bold; font-size:25px; display:flex; flex-direction:row; justify-content:center; align-items:center; background-color:#6600ff; color:#d9d9d9; border-radius:5px; border:1px solid #6600ff; box-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px #d9d9d9;'>CLICK E DIGITE ALGO!</a>");
 	}
 	else if(document.getElementById("caixa2").value == 0){
 		document.write("<a href='index.html' style='text-align:center; text-decoration:none; width:400px; height:80px; font-weight:bold; font-size:25px; display:flex; flex-direction:row; justify-content:center; align-items:center; background-color:#6600ff; color:#d9d9d9; border-radius:5px; border:1px solid #6600ff; box-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px #d9d9d9;'>CLICK E DIGITE 1 Nº INTEIRO DIFERENTE DE ZERO!</a>");
-	}
+	} */
 	
 			/* ALG. SOMA*/
 			/* document.location.reload(true); */
 			var num1 = 0;			
-			var num2 = 0;
-			var num3 = 0;
-			var num4 = 0;
-			var media = 0;
-			num1 = parseFloat(aux1);
-			num2 = parseFloat(aux2);
-			num3 = parseFloat(aux3);
-			num4 = parseFloat(aux4);
-			media = ((num1 + num2 + num3 + num4) / 4);
-
-			if(media >= 7){
-				p1.insertAdjacentHTML("afterend",`<p class='bgResult'><font class='texto1'>A média do aluno = ${media}, portanto o aluno está aprovado!</font></p>`);
-			}
-			else if(media < 7 && media >= 5){
-				p1.insertAdjacentHTML("afterend",`<p class='bgResult'><font class='texto1'>A média do aluno = ${media}, portanto o aluno está de recuperação!</font></p>`);
-			}
-			else{
-				p1.insertAdjacentHTML("afterend",`<p class='bgResult'><font class='texto1'>A média do aluno = ${media}, portanto o aluno está reprovado!</font></p>`);
-			}
+			var soma = 0;
+			var aux2 = 0;
+			var aux3 = 1;
+			num1 = parseInt(aux1);
+			aux2 = num1;
+			soma += (aux2 + 1);			
 			
+			while(soma < 200){
+				p1.insertAdjacentHTML("afterend",`<p class='bgResult'>
+					<font class='texto1'>
+					${num1} + ${aux3} = ${soma}<br />
+					</font>
+				</p>`);			
+				soma++;
+				aux3++;				
+			}
+						
 			
 			//num2 = parseFloat(aux2);
 			
